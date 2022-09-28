d19d23c56141f229180c336ff13d5131bde87787
Renato Mendes <mendeslrenato@gmail.com>
2022-09-28 16:41:08 -0300

    write tests to /chocolates/:id route

--- tests/integration/chocolates.test.js
+++ tests/integration/chocolates.test.js
@@ -9,22 +9,48 @@ const mockFile = require('../mocks/mockCacaoTrybeFIle');
 
 chai.use(chaiHttp);
 
-describe('GET method on /chocolates route', function () {
-  sinon.stub(fs.promises, 'readFile')
+describe('Testing cacaoTrybe API', function () {
+  beforeEach(function () {
+    sinon.stub(fs.promises, 'readFile')
     .resolves(mockFile);
-  it('Should return a list of all chocolates', async function () {
-    const output = [
-      { id: 1, name: 'Mint Intense', brandId: 1 },
-      { id: 2, name: 'White Coconut', brandId: 1 },
-      { id: 3, name: 'Mon Chéri', brandId: 2 },
-      { id: 4, name: 'Mounds', brandId: 3 },
-    ];
-
-    response = await chai
-      .request(app)
-      .get('/chocolates');
-    
-    expect(response.status).to.equal(200);
-    expect(response.body.chocolates).to.deep.equal(output);
   });
-});
+
+  afterEach(function () {
+    sinon.restore();
+  });
+
+  describe('GET method on /chocolates route', function () {
+    it('Should return a list of all chocolates', async function () {
+      const output = [
+        { id: 1, name: 'Mint Intense', brandId: 1 },
+        { id: 2, name: 'White Coconut', brandId: 1 },
+        { id: 3, name: 'Mon Chéri', brandId: 2 },
+        { id: 4, name: 'Mounds', brandId: 3 },
+      ];
+  
+      response = await chai
+        .request(app)
+        .get('/chocolates');
+      
+      expect(response.status).to.equal(200);
+      expect(response.body.chocolates).to.deep.equal(output);
+    });
+  });
+
+  describe('GET method on /chocolates/:id', function () {
+    it('Should return one chocolate matching de id passed through params', async function () {
+      const output = [{
+        id: 4,
+        name: 'Mounds',
+        brand: 3
+      }];
+
+      response = await chai
+        .request(app)
+        .get('/chocolates/4');
+
+      expect(response.status).to.equal(200);
+      expect(response.body.chocolates).to.deep.equal(output);
+    })
+  })
+})
